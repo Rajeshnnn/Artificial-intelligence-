f16c88258c79b8166b02062565e272817ea461c6
Rajeshnnn <146874027+Rajeshnnn@users.noreply.github.com>
2023-10-18 15:07:02 +0530

    Update chat.py

--- chat.py
+++ chat.py
@@ -1,32 +1,15 @@
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.naive_bayes import MultinomialNB
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
-
-# Sample data (replace this with your actual data)
-X = ["Hello, this is a legitimate email.",
-     "Congratulations! You've won a million dollars!",
-     "Meeting scheduled for tomorrow.",
-     "Claim your prize now! Limited time offer!"]
-
-y = [0, 1, 0, 1]  # 0 for ham, 1 for spam
-
-# Convert text data to numerical features using CountVectorizer
-vectorizer = CountVectorizer()
-X_vectorized = vectorizer.fit_transform(X)
-
-# Split the data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X_vectorized, y, test_size=0.2, random_state=42)
-
-# Choose a machine learning model (Naive Bayes)
-model = MultinomialNB()
-
-# Train the model
-model.fit(X_train, y_train)
-
-# Make predictions on the test set
-predictions = model.predict(X_test)
-
-# Calculate accuracy
-accuracy = accuracy_score(y_test, predictions)
-print("Accuracy:", accuracy)
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import classification_report,accuracy_score
+from sklearn.metrics import confusion_matrix
+from tensorflow.keras.layers import TextVectorization
+from sklearn.metrics import precision_score, recall_score, f1_score
+import tensorflow_hub as hub
